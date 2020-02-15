538f0ece072ea8e13f7f8f1cc351801cae438be9
EC2 Default User <ec2-user@ip-172-31-5-204.us-west-1.compute.internal>
2020-02-15 10:34:44 +0000

    Add connection to database

--- code/data.php
+++ code/data.php
@@ -1,5 +1,42 @@
 <?php
 
-echo "{\"productList\": []}";
+$myObj = new stdClass();
+$myObj->productList =
+ array(new stdClass(),new stdClass());
+
+$myObj->productList[0]->product = 
+ "Forte Michelangelo";
+$myObj->productList[0]->price =
+ 0;
+$myObj->productList[1]->product = 
+ "Porto Traiano";
+$myObj->productList[1]->price =
+ 12;
+
+
+$servername = "visitacivitavecchia_db_1";
+$username = "user";
+$password = "password";
+$dbname = "db";
+$conn = $mysqli($servername,
+               $username,
+               $password,
+               $dbname);
+if ($conn->connection_error) {
+    die(
+        "Connection failed: " . 
+        $conn->connection_error);
+}
+
+$result = $conn->query(
+    "SELECT product,price FROM `products`");
+
+if ($result) {
+    foreach($result as $row) {
+        // fill the obj
+    }
+}
+
+echo json_encode($myObj);
 
 ?>
